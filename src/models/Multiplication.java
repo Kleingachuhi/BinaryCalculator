ffe2ba97695f247ba05677527610797f127abadf
Klein Kiuvu Gachuhi <klein.gachuhi@student.moringaschool.com>
2026-01-27 23:16:41 +0300

    Multiplication operation

--- src/models/Multiplication.java
+++ src/models/Multiplication.java
@@ -8,7 +8,7 @@ public class Multiplication implements BinaryOperation {
         String result = "0";
 
         for  (int i = secondBinary.length() - 1; i>-1; i--) {
-            char currentBit = SecondBinary = secondBinary.charAt(i);
+            char currentBit  = secondBinary.charAt(i);
 
             if (currentBit == '1') { 
                 String shiftedValue = firstBinary;
@@ -16,7 +16,7 @@ public class Multiplication implements BinaryOperation {
                 for (int j  =0; j < secondBinary.length() - 1-i; j++) {
                     shiftedValue = shiftedValue + "0";
                 }
-                result = addbinary(result, shiftedValue);
+                result = addBinary(result, shiftedValue);
             }
         }
 return result;
@@ -27,7 +27,38 @@ return result;
         return "*";
     }
 
-    private string addBinary(String firstBinary, string secondBinary){
+    private String addBinary(String firstBinary, String secondBinary){
 
+
+        int maximumLenghth = Math.max(firstBinary.length(), secondBinary.length());
+        firstBinary = paddingWithZeros(firstBinary, maximumLenghth);
+        secondBinary = paddingWithZeros(secondBinary, maximumLenghth);
+
+        StringBuilder result = new StringBuilder();
+        int remainder = 0;
+
+        for (int i = maximumLenghth - 1; i > -1 ; i--) {
+            int unoBit = firstBinary.charAt(i) - '0';
+            int twoBit = secondBinary.charAt(i) - '0';
+
+            int sumTotal = unoBit + twoBit + remainder;
+
+            result.insert(0, sumTotal %2);
+            remainder  = sumTotal / 2;
+        }
+
+        if (remainder == 1) {
+            result.insert(0, "1");
+        }
+
+        return result.toString();
+    }
+    private String paddingWithZeros(String value, int lenght) {
+        StringBuilder paddedBinary = new StringBuilder(value);
+
+        while (paddedBinary.length() < lenght) {
+            paddedBinary.insert(0, "0");
+        }
+        return paddedBinary.toString();
     }
 }
