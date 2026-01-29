6eb3ef5f441a876292bcccdceec4cafc3b6f8073
Klein Kiuvu Gachuhi <klein.gachuhi@student.moringaschool.com>
2026-01-29 13:26:57 +0300

    Validator- check on inputs are valid

--- src/utilities/Validator.java
+++ src/utilities/Validator.java
@@ -2,4 +2,22 @@ package utilities;
 
 public class Validator {
     
+    public static void validateBinary( String binary) throws Exception {
+        if (binary == null || binary.isEmpty()) {
+            throw new Exception("PLease check, Input cannot be empty!");
+        }
+
+        for (int i = 0; i < binary.length(); i++){
+            char currentCharacter = binary.charAt(i);
+
+            if (currentCharacter != '0'  && currentCharacter != '1') {
+                throw new Exception(" Invalid binary number. Either 1 or 0");
+            }
+        }
+    }
+
+    public static void validateInputs (String firstBinary, String secondBinary) throws Exception{
+        validateBinary(firstBinary);
+        validateBinary(secondBinary);
+    }
 }
